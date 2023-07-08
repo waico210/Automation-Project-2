12820e14d2dd9c8fcd2b4d4185587b1dfad6a380
waico210 <waico210@gmail.com>
2023-07-08 17:40:51 +0300

    My commets about the commit

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -59,8 +59,58 @@ describe('Issue create', () => {
  });
 
 //ASSIGNMENT 2 Test 2
-  xit('Create new issue with random data and validate it', () => {
+  it('Create new issue with random data and validate it', () => {
+    const title = faker.lorem.word();
+    const description = faker.lorem.sentence(2);
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+      //Description field
+      cy.get('.ql-editor').type(description);
+      //Title field
+      cy.get('input[name="title"]').type(title);
+
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Low"]').click();
+
+      //Select Baby Yoda from reporter dropdown
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+      //Select Baby Yoda from assignee dropdown
+      cy.get('[data-testid="select:userIds"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+
+      //check if the issue type is a task
+      cy.get('[data-testid="select:type"]').should('contain', 'Task');
+    
+      //Click on button "Create issue"
+      cy.get('button[type="submit"]').click();
+    
+    });
+      //Assert that modal window is closed and successful message is visible
+      cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+      cy.contains('Issue has been successfully created.').should('be.visible');
+      
+      //Reload the page to be able to see recently created issue
+      //Assert that successful message has dissappeared after the reload
+      cy.reload();
+      cy.contains('Issue has been successfully created.').should('not.exist');
+    
+      //Assert than only one list with name Backlog is visible and do steps inside of it
+      cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+        //Assert that this list contains 5 issues and first element with tag p has specified text
+        cy.get('[data-testid="list-issue"]')
+            .should('have.length', '5')
+            .first()
+            .find('p')
+            .contains(title);
+        //Assert that correct avatar and type icon are visible
+        cy.get('[data-testid="avatar:Baby Yoda"]').should('be.visible');
+        cy.get('[data-testid="icon:task"]').should('be.visible');
+      });
+    
+
   });
+
+  
 
 
   xit('Should create an issue and validate it successfully', () => {
